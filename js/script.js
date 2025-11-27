e4526f4309cf54f0adb5f4fa2945dd11de1df74b
bhaskar28051975-ctrl <bhaskar28051975@gmail.com>
2025-11-27 10:04:20 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+// AUTO BANNER SYSTEM FOR DRIVER PAGES
 
+// Detect current page file name
+let page = window.location.pathname.split("/").pop();
+
+// Change banner based on driver page
+if (page.includes("driver1")) {
+    document.getElementById("bannerImage").src = "images/banner-driver1.jpg";
+}
+
+else if (page.includes("driver2")) {
+    document.getElementById("bannerImage").src = "images/banner-driver2.jpg";
+}
+
+else if (page.includes("driver3")) {
+    document.getElementById("bannerImage").src = "images/banner-driver3.jpg";
+}
+
+// Default banner for all other pages
+else {
+    document.getElementById("bannerImage").src = "images/banner-default.jpg";
+}
